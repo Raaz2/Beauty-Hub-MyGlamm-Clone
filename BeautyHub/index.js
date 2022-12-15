607f760f5a5d57919761f736f10be80c08f0a952
Raj Mishra <rajeevranjanmishra4339@gmail.com>
2022-12-15 21:55:54 +0530

    day-2

---
+++ BeautyHub/index.js
@@ -0,0 +1,11 @@
+
+// let images = document.querySelector("#carousel").querySelectorAll("img");
+// i = 0;
+// setInterval(() => {
+//     if (i == images.length) {
+//         i = 0;
+//     }
+//     document.querySelector("#carousel").append(images[i++])
+    
+// }, 2000);
+
