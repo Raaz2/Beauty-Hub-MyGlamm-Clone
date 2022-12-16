7737e072524c5dabea18cca50e686b081edb8920
Raj Mishra <rajeevranjanmishra4339@gmail.com>
2022-12-17 00:23:10 +0530

    day-4

--- BeautyHub/index.js
+++ BeautyHub/index.js
@@ -1,20 +1,13 @@
 
-// let images = document.querySelector("#carousel").querySelectorAll("img");
-// i = 0;
-// setInterval(() => {
-//     if (i == images.length) {
-//         i = 0;
-//     }
-//     document.querySelector("#carousel").append(images[i++])
-    
-// }, 2000);
-
 function popup(){
     // document.querySelector("body").style.position="absolute"
     document.querySelector(".border").style.display="block"
 }
-document.querySelector(".order>a").addEventListener("click",fun)
-function fun(event){
+
+document.querySelector("#signin").addEventListener("click",signin)
+function signin(event){
     event.preventDefault()
-    document.querySelector(".border").style.display="none"
+    let mob=document.querySelector("#mob").value;
+    window.location.href="./signupnext.html"
+    localStorage.setItem("mobile",mob)
 }
